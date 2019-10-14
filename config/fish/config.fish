5df3a48628b696a6546f6f80c1cc12d53de989df
nrxr <nrxr@disroot.org>
2019-10-13 20:04:32 -0400

    config.fish: change to use $PATH
    Usage of $fish_user_paths with -U is slow, really slow. Change to $PATH
    so it loads swiftly.
    
    See: https://github.com/fish-shell/fish-shell/issues/2688

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,36 +18,36 @@ end
 # setting up paths
 #
 # the standards
-set -U fish_user_paths $fish_user_paths /usr/local/{bin/sbin}
+set -x PATH $PATH /usr/local/{bin/sbin}
 
 # on macOS, install the /usr/local paths for homebrew
 if test $os = 'mac'
-  set -U fish_user_paths $fish_user_paths /usr/local/opt/{coreutils,gnu-sed}/libexec/gnubin \
+  set -x PATH $PATH /usr/local/opt/{coreutils,gnu-sed}/libexec/gnubin \
     /usr/local/opt/git/share/git-core/contrib/diff-highlight
 
   if test -d /usr/local/opt/icu4c/bin
-    set -U fish_user_paths $fish_user_paths /usr/local/opt/icu4c/{bin,sbin}
+    set -x PATH $PATH /usr/local/opt/icu4c/{bin,sbin}
   end
 
   if test -d /usr/texbin
-    set -U fish_user_paths $fish_user_paths /usr/texbin
+    set -x PATH $PATH /usr/texbin
   end
 end
 
 # on linux load some special paths
 if test $os = 'linux'
   if test -f /usr/share/doc/git/contrib/diff-highlight/diff-highlight
-    set -U fish_user_paths $fish_user_paths /usr/share/doc/git/contrib/diff-highlight
+    set -x PATH $PATH /usr/share/doc/git/contrib/diff-highlight
   end
 
   if test -f /usr/share/git/diff-highlight/diff-highlight
-    set -U fish_user_paths $fish_user_paths /usr/share/git/diff-highlight
+    set -x PATH $PATH /usr/share/git/diff-highlight
   end
 end
 
 # the go-based
 if test -d ~/code/bin
-  set -U fish_user_paths $fish_user_paths ~/code/bin
+  set -x PATH $PATH ~/code/bin
 end
 
 if test -f ~/.config/fish/private
