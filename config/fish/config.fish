a9b51b17662edd04021479374935b8c2b6695934
nrxr <git@nrxr.org>
2025-09-18 00:12:02 -0300

    feat: remove fisher and pure
    Since Starship is the new prompt being used, now pure is no longer
    needed and fisher is only used for managing pure.
    
    The hooks/post-up 01 and 02 are removed, and the check for the existence
    of fisher in fish/config.fish.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,3 @@
-# make sure fisher is installed
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 if functions -q _nrxr_private_dir_envs
   _nrxr_private_dir_envs
 end
